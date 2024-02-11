a6046695f567e3e7c9d44a2973b084acc3d609c4
gauravv486 <154688248+gauravv486@users.noreply.github.com>
2024-02-11 18:45:06 +0530

    Update script.js
    main input disabled while editing one task

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const taskInput = document.getElementById("taskInput");
 const taskcontent = document.getElementById("To-Do-list");
 
 function createlement() {
+
     if (taskInput.value === "") {
         alert("You Haven't Write your Task");
     } else {
@@ -28,7 +29,7 @@ function createlement() {
         });
 
         let savebutton = document.createElement("button");
-        savebutton.id = "deletebtn";
+        savebutton.id = "savebtn";
         savebutton.textContent = "save";
         savebutton.style.display="none";
         savebutton.addEventListener("click", function () {
@@ -51,16 +52,26 @@ function deleteclick(deleteButton) {
     deleteButton.parentElement.remove();
 }
 
+
 function editclick(editButton,savebutton,deleteButton) {
+
     let currentbutton=editButton.parentElement.querySelector("#editbtn");
     let taskElement = editButton.parentElement.querySelector("#taskelement");
     let editbuttons=document.querySelectorAll("#editbtn");
+    let deletebuttons=document.querySelectorAll("#deletebtn");
     let inputbutton=document.querySelectorAll("#taskelement");
-
-
-
+    let maintask=document.querySelector("#taskInput");
 
     editbuttons.forEach(element => {
+        if(element!==currentbutton){
+            element.style.display="none";
+        }
+        else {
+            currentbutton.focus();
+        }
+    });
+
+    deletebuttons.forEach(element => {
         if(element!==currentbutton){
             element.style.display="none";
         }
@@ -73,27 +84,40 @@ function editclick(editButton,savebutton,deleteButton) {
         if(element!=taskElement)
         {
             element.readOnly=true;
+            element.style.height="55px";
         }
         else{
             taskElement.readOnly = false;
         }
     })
 
-    // taskElement.readOnly = false;
     taskElement.focus();
     editButton.style.display = "none";
     deleteButton.style.display="none";
     savebutton.style.display = "flex";
-} 
+    maintask.disabled=true;
 
+} 
 
 function saveclick(editbtn,savebutton,deleteButton){
 
+    let maintask=document.querySelector("#taskInput");
     let editbtnshow=document.querySelectorAll("#editbtn");
+    let deletetnshow=document.querySelectorAll("#deletebtn");
+    let inputbutton=document.querySelectorAll("#taskelement");
+
+    inputbutton.forEach(element =>{
+        element.removeAttribute("height");
+    })
+
     editbtnshow.forEach(element =>{
+        element.style.display="flex";
+    })
+    deletetnshow.forEach(element =>{
         element.style.display="flex";
     })
     editbtn.style.display="flex";
     savebutton.style.display="none";
     deleteButton.style.display="flex";
+    maintask.disabled=false;
 }
